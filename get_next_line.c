c2c740bc31546fc05741d53a2c771accd9ad1e88
Guelmis Cortina <gcortina@e1z2r5p25.42.us.org>
2016-11-14 13:35:40 -0800

    clean everythin up of norm errors, still have the two errors showing on moulitest

--- get_next_line.c
+++ get_next_line.c
@@ -6,169 +6,121 @@
 /*   By: gcortina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/29 11:28:56 by gcortina          #+#    #+#             */
-/*   Updated: 2016/10/29 11:28:58 by gcortina         ###   ########.fr       */
+/*   Updated: 2016/11/14 13:30:38 by gcortina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+static int	init_or_join(char **line, char **buf, char **piece_ofstr)
+{
+	if (buf)
+	{
+		if (!line || !(*line = ft_strnew(1)))
+			return (-1);
+		if (!*piece_ofstr && !(*piece_ofstr = ft_strnew(1)))
+			return (-1);
+		if (!(*buf = ft_strnew(BUFF_SIZE)))
+			return (-1);
+	}
+	else
+	{
+		*line = ft_strjoin(*line, *piece_ofstr);
+		ft_memdel((void**)piece_ofstr);
+	}
+	return (0);
+}
+
+static void	add_toline(char **line, char *buf, char **piece_ofstr, char *temp)
+{
+	*temp = '\0';
+	if (buf)
+		*line = ft_strjoin(*line, buf);
+	else
+		*line = ft_strjoin(*line, *piece_ofstr);
+	*piece_ofstr = ft_strdup(temp + 1);
+	temp = NULL;
+	if (!ft_strlen(*piece_ofstr))
+		ft_memdel((void**)piece_ofstr);
+}
+
 static void	check_buf(char **line, char *buf, char **piece_ofstr)
 {
 	char	*temp;
 
-	//printf("%s\n", "Inside check_buf");
 	if (buf && ft_strlen(buf))
 	{
-		//printf("%s\n", "buf has something");
-		//printf("piece_ofstr: %s\n", *piece_ofstr);
-		if (*piece_ofstr && (temp = ft_strchr(*piece_ofstr, '\n')) && ft_strlen(*line) == 0)
-		{
-			*temp = '\0';
-			//printf("piece_ofstr has newline and line is empty piece_ofstr: %s\n", *piece_ofstr);
-			*line = ft_strjoin(*line, *piece_ofstr);
-			//printf("line gets join with piece_ofstr line: %s\n", *line);
-			*piece_ofstr = ft_strdup(temp + 1);
-			//printf("piece_ofstr gets the rest piece_ofstr: %s\n", *piece_ofstr);
-			temp = NULL;
-			if (!ft_strlen(*piece_ofstr))
-			{
-				ft_memdel((void**)piece_ofstr);
-				//printf("piece_ofstr is empty so it gets free here piece_ofstr: %s\n", *piece_ofstr);
-			}
-		}
+		if (*piece_ofstr && (temp = ft_strchr(*piece_ofstr, '\n'
+			&& ft_strlen(*line) == 0)))
+			add_toline(line, NULL, piece_ofstr, temp);
 		else if (ft_strlen(*line) == 0)
-		{
-			*line = ft_strjoin(*line, *piece_ofstr);
-			//printf("piece_ofstr doesnt have a newline so it joins line: %s\n", *line);
-			ft_memdel((void**)piece_ofstr);
-			//printf("piece_ofstr gets free here piece_ofstr: %s\n", *piece_ofstr);			
-		}
+			init_or_join(line, NULL, piece_ofstr);
 		if ((temp = ft_strchr(buf, '\n')))
-		{
-			//printf("buf has a newline buf:\n%s\n", buf);
-			*temp = '\0';
-			*line = ft_strjoin(*line, buf);
-			//printf("buf joins line: %s\n", *line);
-			*piece_ofstr = ft_strdup(temp + 1);
-			//printf("piece_ofstr gets the rest of the buf piece_ofstr: %s\n", *piece_ofstr);
-			temp = NULL;
-			if (!ft_strlen(*piece_ofstr))
-			{
-				ft_memdel((void**)piece_ofstr);
-				//printf("piece_ofstr gets free here piece_ofstr: %s\n", *piece_ofstr);
-			}
-		}
+			add_toline(line, buf, piece_ofstr, temp);
 		else
-		{
 			*line = ft_strjoin(*line, buf);
-			//printf("buf doesnt have a newline so joins line: %s\n", *line);
-		}
 	}
 	else
 	{
-		//printf("buf is empty buf: %s\n", buf);
 		if ((temp = ft_strchr(*piece_ofstr, '\n')))
-		{
-			//printf("piece_ofstr has newline piece_ofstr: %s\n", *piece_ofstr);
-			*temp = '\0';
-			*line = ft_strjoin(*line, *piece_ofstr);
-			//printf("piece_ofstr joins line: %s\n", *line);
-			*piece_ofstr = ft_strdup(temp + 1);
-			//printf("piece_ofstr gets the rest piece_ofstr: %s\n", *piece_ofstr);
-			temp = NULL;
-			if (!ft_strlen(*piece_ofstr))
-			{
-				ft_memdel((void**)piece_ofstr);
-				//printf("piece_ofstr gets free here piece_ofstr: %s\n", *piece_ofstr);
-			}
-		}
+			add_toline(line, NULL, piece_ofstr, temp);
 		else
-		{
-			*line = ft_strjoin(*line, *piece_ofstr);
-			//printf("piece_ofstr does not have newline piece_ofstr: %s\n", *piece_ofstr);
-			//printf("piece_ofstr joins line: %s\n", *line);
-			ft_memdel((void**)piece_ofstr);
-			//printf("piece_ofstr gets free here piece_ofstr: %s\n", *piece_ofstr);			
-		}		
+			init_or_join(line, NULL, piece_ofstr);
 	}
+}
+
+static int	read_file(char *buf, char **line, char **piece_ofstr, int ret)
+{
+	char	*temp;
+
+	if (!ft_strlen(buf))
+		return (-1);
+	buf[ret] = '\0';
+	if (*piece_ofstr && (temp = ft_strchr(*piece_ofstr, '\n')))
+	{
+		add_toline(line, NULL, piece_ofstr, temp);
+		if (!*piece_ofstr && !(*piece_ofstr = ft_strnew(1)))
+			return (-1);
+		*piece_ofstr = ft_strjoin(*piece_ofstr, buf);
+		ft_memdel((void**)&buf);
+		return (1);
+	}
+	else if (ft_strchr(buf, '\n'))
+	{
+		check_buf(line, buf, piece_ofstr);
+		ft_memdel((void**)&buf);
+		return (1);
+	}
+	else
+		check_buf(line, buf, piece_ofstr);
+	return (0);
 }
 
 int			get_next_line(const int fd, char **line)
 {
 	int			ret;
+	int			rd;
 	char		*buf;
-	char		*temp;
 	static char	*piece_ofstr;
 
-	if (!line || !(*line = ft_strnew(1)))
-		return (-1);
-	if (!piece_ofstr)
-	{
-		//printf("piece_ofstr before: %s\n", piece_ofstr);
-		//printf("%s\n", "piece_ofstr gets initialized");
-		if (!(piece_ofstr = ft_strnew(1)))
-			return (-1);
-		//printf("piece_ofstr after: %s\n", piece_ofstr);
-	}
-	if (!(buf = ft_strnew(BUFF_SIZE)))
-		return (-1);
+	if ((rd = init_or_join(line, &buf, &piece_ofstr)))
+		return (rd);
 	if (fd >= 0)
 	{
-		//printf("%s\n", "right before read");
 		while ((ret = read(fd, buf, BUFF_SIZE)))
-		{
-			if (!ft_strlen(buf))
-				return -1;
-			//printf("%s\n", "inside read");
-			//printf("ret: %d\n", ret);
-			buf[ret] = '\0';
-			//printf("buf: %s\n", buf);
-			if (piece_ofstr && (temp = ft_strchr(piece_ofstr, '\n')))
-			{
-				*temp = '\0';	
-				//printf("%s\n", "piece_ofstr has newline");
-				*line = ft_strjoin(*line, piece_ofstr);
-				//printf("line: %s\n", *line);
-				piece_ofstr = ft_strdup(temp + 1);
-				piece_ofstr = ft_strjoin(piece_ofstr, buf);
-				//printf("piece_ofstr: %s\n", piece_ofstr);
-				temp = NULL;
-				ft_memdel((void**)&buf);
-				//printf("buf gets free here buf: %s\n", buf);
-				return (1);
-			}			
-			else if (ft_strchr(buf, '\n'))
-			{
-				//printf("%s\n", "buf has newline");
-				check_buf(line, buf, &piece_ofstr);
-				ft_memdel((void**)&buf);
-				//printf("buf gets free here buf: %s\n", buf);
-				return (1);
-			}
-			else
-			{
-				//printf("%s\n", "no newline in buf");
-				check_buf(line, buf, &piece_ofstr);
-			}
-		}
-		//printf("%s\n", "read is done");
+			if ((rd = read_file(buf, line, &piece_ofstr, ret)))
+				return (rd);
 		ft_memdel((void**)&buf);
-		//printf("buf gets free here buf: %s\n", buf);
 		if (piece_ofstr && ft_strlen(piece_ofstr))
 		{
 			check_buf(line, buf, &piece_ofstr);
-			//printf("line: %s\n", *line);
-			//printf("piece_ofstr: %s\n", piece_ofstr);
 			return (1);
 		}
 		else if (*line && ft_strlen(*line))
 			return (1);
 		else
-		{
-			//printf("%s\n", "now it returns 0");
 			return (0);
-		}
 	}
 	else
 		return (-1);
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,18 @@
 /*   By: gcortina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/29 11:30:01 by gcortina          #+#    #+#             */
-/*   Updated: 2016/10/29 11:30:03 by gcortina         ###   ########.fr       */
+/*   Updated: 2016/11/14 13:31:40 by gcortina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
 # include "libft.h"
-# define BUFF_SIZE 15
+# define BUFF_SIZE 10
 
 int			get_next_line(const int fd, char **line);
 #endif
